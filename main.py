bebd24e0e76a196a485476eebbeedd42b23360b2
Yusif Israfilov <y.israfilov189@mail.ru>
2023-07-15 17:01:14 +0400

    resizable

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import functions as f
 root = Tk()
 root.title("PassGen")
 root.geometry("400x400+500+100")
-root.resizable(False, False)
+root.resizable(True, True)
 root.iconbitmap("icon.ico")
 
 
 btn = ttk.Button(root, text="Generate")
 btn.pack()
 
-label = ttk.Label(text="Hello Tkinter", background="#FFCDD2", foreground="#B71C1C", padding=8)
+label = ttk.Label(text="By Xenyx", background="#FFCDD2", foreground="#B71C1C", padding=8)
 label.pack(expand=True)
 mainloop()
